d6a9d1da944e7894af472716eae0a8fa32eaa4da
Kornel Dubieniecki <lekterable@gmail.com>
2019-02-23 15:29:25 +0100

    Define types

--- server.js
+++ server.js
@@ -1,16 +1,38 @@
 const { ApolloServer, gql } = require('apollo-server')
 
 const typeDefs = gql`
+  scalar Date
+
+  enum ExpenseType {
+    FOOD
+    BILLS
+    ENTERTAINMENT
+    OTHER
+  }
+
   type Expense {
     id: String!
+    date: Date!
+    description: String
+    type: ExpenseType!
+    householder: Householder!
+    household: Household!
+    cost: Int!
   }
 
   type Householder {
     id: String!
+    name: String!
+    expenses: [Expense]
+    households: [Household]
   }
 
   type Household {
     id: String!
+    owner: Householder!
+    name: String!
+    expenses: [Expense]
+    householders: [Householder]
   }
 
   type Query {
